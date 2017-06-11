760d4ec25ae16d961ec278ec1073864285e35e6b
MikeBoris <mikeboris123@gmail.com>
2017-06-11 11:47:44 -0400

    Tidied up html

--- app.py
+++ app.py
@@ -16,14 +16,24 @@ def form():
 # accepting: POST requests in this case
 @app.route('/hello/', methods=['POST'])
 def hello():
-    number=float(request.form['number'])
-    cats=float(request.form['cats'])
-    r1=float(request.form['rad_1'])
-    r2=float(request.form['rad_2'])
-    r3=float(request.form['rad_3'])
-    result=number*cats*r3*r2*r1
-    result_string="You've got " + str(result) + " cats!!"
-    return render_template('form_submit.html', result_string=result_string)
+	if request.method == 'POST':
+		number=int(request.form['number'])
+		cats=int(request.form['cats'])
+		r1=int(request.form['rad_1'])
+		r2=int(request.form['rad_2'])
+		r3=int(request.form['rad_3'])
+		result=number*cats*r3*r2*r1
+		result_string="You've got " + str(result) + " cats!!"
+		return render_template('form_submit.html', result_string=result_string)
+	else:
+		number=int(request.args.get('number'))
+		cats=int(request.args.get('cats'))
+		r1=int(request.args.get('rad_1'))
+		r2=int(request.args.get('rad_2'))
+		r3=int(request.args.get('rad_3'))
+		result=number*cats*r3*r2*r1
+		result_string="You've got " + str(result) + " cats!!"
+		return render_template('form_submit.html', result_string=result_string)		
 
 # Run the app
 if __name__ == '__main__':
